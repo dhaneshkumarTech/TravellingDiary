6a0bf7f01481da55e935685e3754ea5c5a587d08
Dhanesh Kumar <dmeghwar.bscs18seecs@seecs.edu.pk>
2022-10-30 04:12:22 +0500

    Components (Navbar: Containing nav-logo and nav-title)

---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Navbar() {
+    return (
+        <nav>
+            <img className='nav-logo' src='images/diary.png'></img>
+            <span className='nav-title'>My Travel Journal</span>
+        </nav>
+    )
+}
+export default Navbar;
